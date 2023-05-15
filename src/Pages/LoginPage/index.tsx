a4435bb3f3fb3403498efd27b8357582d9d54354
Eduardo Soares <eduardopbs234@outlook.com>
2023-05-15 16:58:53 -0300

    inputs e useForm

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -1,16 +1,33 @@
-import Input from "Components/Input";
 import Button from "Components/Button";
 import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (e) => {
+        console.log("Fazendo Login")
+        console.log(e)
+    };
+
     return (
         <div className='flex justify-center font-manrope w-full lg:px-64 xl:px-96 py-16'>
-            <form className='flex flex-col gap-4 sm:justify-between  bg-main-blue px-12 py-3 border-2 border-main-green rounded-xl'>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className='flex flex-col gap-4 sm:justify-between  bg-main-blue px-12 py-3 border-2 border-main-green rounded-xl'
+                >
                 <label className='text-main-green text-2xl font-manrope text-center mb-4 uppercase font-bold border-b-2 border-main-green'>
                     Login
                 </label>
-                <Input text='Email' type='email' placeholder='Email' />
-                <Input text='Senha' type='password' placeholder='Senha' />
+                <div>
+                    <label className="text-white">Email</label>
+                    <input {...register("email", {required: true})} className="main-input" type='email' placeholder='Email' />
+                </div>
+                <div>
+                    <label className="text-white">Senha</label>
+                    <input {...register("senha", {required: true })}className="main-input" type='password' placeholder='Senha' />
+                </div>
                 <Button primary={true} text='Entrar' />
                 <Link to='/register' className='text-white w-max'>
                     Ou{" "}
